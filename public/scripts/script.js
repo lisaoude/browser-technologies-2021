b1f0eb13702c9e468bb077f1645d436ed6ea4f4b
Lisa Oude Elferink <lisaoudeelferink@hotmail.com>
2021-03-29 17:03:35 +0200

    :heavy_plus_sign: Added comments

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,37 +1,48 @@
 window.onload = () => {
+
+  // show localStorage in input fields
   populateValues();
 
+  // select all 'buttons' 
   const buttons = document.querySelectorAll('a');
 
+  // for each button, listen to on click event
+  // and execute function
   buttons.forEach(button => {
     button.onclick = onButtonClick;
   })
 };
 
-const onButtonClick = (e) => {
+const onButtonClick = (event) => {
 
-  // de geklikte button
-  const button = e.currentTarget;
+  // select the clicked button
+  const button = event.currentTarget;
 
-  // de section waar ie in zit
+  // select the section in which that button is located
   const section = button.parentElement;
 
-  // alle inputs in de section
+  // select all input fields inside that section
   const inputs = section.querySelectorAll('input');
 
   inputs.forEach(input => {
 
-    // als er iets in staat, dan doen
+    // if there's a value, then put it in localStorage
     if (input.name)
       localStorage.setItem(input.name, input.value);
   })
 };
 
 const populateValues = () => {
+
+  // select all input fields on the page
   const inputs = document.querySelectorAll('input');
+
   inputs.forEach(input => {
+
+    // get the localStorage values
     const item = localStorage.getItem(input.name)
 
+    // if there's an item, put it in the input field 
     if (item) input.value = item;
   })
 };
