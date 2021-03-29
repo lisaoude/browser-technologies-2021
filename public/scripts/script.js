a61e889d037cb894e123114da496164273a62fab
Lisa Oude Elferink <lisaoudeelferink@hotmail.com>
2021-03-29 14:12:50 +0200

    :heavy_check_mark: fixed localStorage & made code about 10 times shorter (thanks Jonah)

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,343 +1,37 @@
-//////////////// Variables
+window.onload = () => {
+  populateValues();
 
-// 'login' variables for localStorage
-const naamKey = document.querySelector("#naamKey");
-const naamValue = document.querySelector("#naam");
-const studentnummerKey = document.querySelector("#studentnummerKey");
-const studentnummerValue = document.querySelector("#studentnummer");
-const loginButton = document.querySelector("#loginButton");
+  const buttons = document.querySelectorAll('a');
 
-
-// WAFS variables for localStorage
-const wafsDocentKey = document.querySelector("#WAFSdocentKey");
-const wafsDocentValue = document.querySelector("#WAFSdocent");
-
-const wafsWeken14Key = document.querySelector("#WAFSweken1-4Key");
-const wafsWeken14Value = document.querySelector("#WAFSweken1-4");
-
-const wafsWeken59Key = document.querySelector("#WAFSweken5-9Key");
-const wafsWeken59Value = document.querySelector("#WAFSweken5-9");
-
-const wafsWeken1014Key = document.querySelector("#WAFSweken10-14Key");
-const wafsWeken1014Value = document.querySelector("#WAFSweken10-14");
-
-const wafsMoeilijkKey = document.querySelector("#WAFSmoeilijkKey");
-const wafsMoeilijkValue = document.querySelector("#WAFSmoeilijk");
-
-const wafsUitlegKey = document.querySelector("#WAFSuitlegKey");
-const wafsUitlegValue = document.querySelector("#WAFSuitleg");
-
-const wafsSnappenKey = document.querySelector("#WAFSsnappenKey");
-const wafsSnappenValue = document.querySelector("#WAFSsnappen");
-
-const wafsCijferKey = document.querySelector("#WAFScijferKey");
-const wafsCijferValue = document.querySelector("#WAFScijfer");
-
-
-// CSSTTR variables for localStorage
-const cssttrDocentKey = document.querySelector("#CSSTTRdocentKey");
-const cssttrDocentValue = document.querySelector("#CSSTTRdocent");
-
-const cssttrWeken14Key = document.querySelector("#CSSTTRweken1-4Key");
-const cssttrWeken14Value = document.querySelector("#CSSTTRweken1-4");
-
-const cssttrWeken59Key = document.querySelector("#CSSTTRweken5-9Key");
-const cssttrWeken59Value = document.querySelector("#CSSTTRweken5-9");
-
-const cssttrWeken1014Key = document.querySelector("#CSSTTRweken10-14Key");
-const cssttrWeken1014Value = document.querySelector("#CSSTTRweken10-14");
-
-const cssttrMoeilijkKey = document.querySelector("#CSSTTRmoeilijkKey");
-const cssttrMoeilijkValue = document.querySelector("#CSSTTRmoeilijk");
-
-const cssttrUitlegKey = document.querySelector("#CSSTTRuitlegKey");
-const cssttrUitlegValue = document.querySelector("#CSSTTRuitleg");
-
-const cssttrSnappenKey = document.querySelector("#CSSTTRsnappenKey");
-const cssttrSnappenValue = document.querySelector("#CSSTTRsnappen");
-
-const cssttrCijferKey = document.querySelector("#CSSTTRcijferKey");
-const cssttrCijferValue = document.querySelector("#CSSTTRcijfer");
-
-
-// PWA variables for localStorage
-const pwaDocentKey = document.querySelector("#PWAdocentKey");
-const pwaDocentValue = document.querySelector("#PWAdocent");
-
-const pwaWeken14Key = document.querySelector("#PWAweken1-4Key");
-const pwaWeken14Value = document.querySelector("#PWAweken1-4");
-
-const pwaWeken59Key = document.querySelector("#PWAweken5-9Key");
-const pwaWeken59Value = document.querySelector("#PWAweken5-9");
-
-const pwaWeken1014Key = document.querySelector("#PWAweken10-14Key");
-const pwaWeken1014Value = document.querySelector("#PWAweken10-14");
-
-const pwaMoeilijkKey = document.querySelector("#PWAmoeilijkKey");
-const pwaMoeilijkValue = document.querySelector("#PWAmoeilijk");
-
-const pwaUitlegKey = document.querySelector("#PWAuitlegKey");
-const pwaUitlegValue = document.querySelector("#PWAuitleg");
-
-const pwaSnappenKey = document.querySelector("#PWAsnappenKey");
-const pwaSnappenValue = document.querySelector("#PWAsnappen");
-
-const pwaCijferKey = document.querySelector("#PWAcijferKey");
-const pwaCijferValue = document.querySelector("#PWAcijfer");
-
-
-// BT variables for localStorage
-const btDocentKey = document.querySelector("#BTdocentKey");
-const btDocentValue = document.querySelector("#BTdocent");
-
-const btWeken14Key = document.querySelector("#BTweken1-4Key");
-const btWeken14Value = document.querySelector("#BTweken1-4");
-
-const btWeken59Key = document.querySelector("#BTweken5-9Key");
-const btWeken59Value = document.querySelector("#BTweken5-9");
-
-const btWeken1014Key = document.querySelector("#BTweken10-14Key");
-const btWeken1014Value = document.querySelector("#BTweken10-14");
-
-const btMoeilijkKey = document.querySelector("#BTmoeilijkKey");
-const btMoeilijkValue = document.querySelector("#BTmoeilijk");
-
-const btUitlegKey = document.querySelector("#BTuitlegKey");
-const btUitlegValue = document.querySelector("#BTuitleg");
-
-const btSnappenKey = document.querySelector("#BTsnappenKey");
-const btSnappenValue = document.querySelector("#BTsnappen");
-
-const btCijferKey = document.querySelector("#BTcijferKey");
-const btCijferValue = document.querySelector("#BTcijfer");
-
-
-// RTW variables for localStorage
-const rtwDocentKey = document.querySelector("#RTWdocentKey");
-const rtwDocentValue = document.querySelector("#RTWdocent");
-
-const rtwWeken14Key = document.querySelector("#RTWweken1-4Key");
-const rtwWeken14Value = document.querySelector("#RTWweken1-4");
-
-const rtwWeken59Key = document.querySelector("#RTWweken5-9Key");
-const rtwWeken59Value = document.querySelector("#RTWweken5-9");
-
-const rtwWeken1014Key = document.querySelector("#RTWweken10-14Key");
-const rtwWeken1014Value = document.querySelector("#RTWweken10-14");
-
-const rtwMoeilijkKey = document.querySelector("#RTWmoeilijkKey");
-const rtwMoeilijkValue = document.querySelector("#RTWmoeilijk");
-
-const rtwUitlegKey = document.querySelector("#RTWuitlegKey");
-const rtwUitlegValue = document.querySelector("#RTWuitleg");
-
-const rtwSnappenKey = document.querySelector("#RTWsnappenKey");
-const rtwSnappenValue = document.querySelector("#RTWsnappen");
-
-const rtwCijferKey = document.querySelector("#RTWcijferKey");
-const rtwCijferValue = document.querySelector("#RTWcijfer");
-
-
-// HCD variables for localStorage
-const hcdDocentKey = document.querySelector("#HCDdocentKey");
-const hcdDocentValue = document.querySelector("#HCDdocent");
-
-const hcdWeken14Key = document.querySelector("#HCDweken1-4Key");
-const hcdWeken14Value = document.querySelector("#HCDweken1-4");
-
-const hcdWeken59Key = document.querySelector("#HCDweken5-9Key");
-const hcdWeken59Value = document.querySelector("#HCDweken5-9");
-
-const hcdWeken1014Key = document.querySelector("#HCDweken10-14Key");
-const hcdWeken1014Value = document.querySelector("#HCDweken10-14");
-
-const hcdMoeilijkKey = document.querySelector("#HCDmoeilijkKey");
-const hcdMoeilijkValue = document.querySelector("#HCDmoeilijk");
-
-const hcdUitlegKey = document.querySelector("#HCDuitlegKey");
-const hcdUitlegValue = document.querySelector("#HCDuitleg");
-
-const hcdSnappenKey = document.querySelector("#HCDsnappenKey");
-const hcdSnappenValue = document.querySelector("#HCDsnappen");
-
-const hcdCijferKey = document.querySelector("#HCDcijferKey");
-const hcdCijferValue = document.querySelector("#HCDcijfer");
-
-
-
-
-
-
-//////////////// localStorage
-
-//////// 'login' localStorage
-loginButton.onclick = () => {
-
-  // label text - KEY
-  const naam = naamKey.textContent;
-  const studentnummer = studentnummerKey.textContent;
-
-  // input value - VALUE
-  const valueNaam = naamValue.value;
-  const valueStudentnummer = studentnummerValue.value;
-
-  // local storage
-  localStorage.setItem(naam, valueNaam);
-  localStorage.setItem(studentnummer, valueStudentnummer);
+  buttons.forEach(button => {
+    button.onclick = onButtonClick;
+  })
 };
 
+const onButtonClick = (e) => {
 
-//////// WAFS localStorage
-wafsButton.onclick = () => {
+  // de geklikte button
+  const button = e.currentTarget;
 
-  // label text - KEY
-  const wafsDocent = 'WAFS docent';
-  const wafsMoeilijk = 'WAFS moeilijk';
-  const wafsUitleg = 'WAFS uitleg';
-  const wafsSnappen = 'WAFS snappen';
-  const wafsCijfer = 'WAFS cijfer';
+  // de section waar ie in zit
+  const section = button.parentElement;
 
-  // input value - VALUE
-  const valueWafsDocent = wafsDocentValue.value;
-  const valueWafsMoeilijk = wafsMoeilijkValue.value;
-  const valueWafsUitleg = wafsUitlegValue.value;
-  const valueWafsSnappen = wafsSnappenValue.value;
-  const valueWafsCijfer = wafsCijferValue.value;
+  // alle inputs in de section
+  const inputs = section.querySelectorAll('input');
 
-  // local storage
-  localStorage.setItem(wafsDocent, valueWafsDocent);
-  localStorage.setItem(wafsMoeilijk, valueWafsMoeilijk);
-  localStorage.setItem(wafsUitleg, valueWafsUitleg);
-  localStorage.setItem(wafsSnappen, valueWafsSnappen);
-  localStorage.setItem(wafsCijfer, valueWafsCijfer);
+  inputs.forEach(input => {
+
+    // als er iets in staat, dan doen
+    if (input.name)
+      localStorage.setItem(input.name, input.value);
+  })
 };
 
+const populateValues = () => {
+  const inputs = document.querySelectorAll('input');
+  inputs.forEach(input => {
+    const item = localStorage.getItem(input.name)
 
-//////// CSSTTR localStorage
-cssttrButton.onclick = () => {
-
-  // label text - KEY
-  const cssttrDocent = 'CSSTTR docent';
-  const cssttrMoeilijk = 'CSSTTR moeilijk';
-  const cssttrUitleg = 'CSSTTR uitleg';
-  const cssttrSnappen = 'CSSTTR snappen';
-  const cssttrCijfer = 'CSSTTR cijfer';
-
-  // input value - VALUE
-  const valueCssttrDocent = cssttrDocentValue.value;
-  const valueCssttrMoeilijk = cssttrMoeilijkValue.value;
-  const valueCssttrUitleg = cssttrUitlegValue.value;
-  const valueCssttrSnappen = cssttrSnappenValue.value;
-  const valueCssttrCijfer = cssttrCijferValue.value;
-
-  // local storage
-  localStorage.setItem(cssttrDocent, valueCssttrDocent);
-  localStorage.setItem(cssttrMoeilijk, valueCssttrMoeilijk);
-  localStorage.setItem(cssttrUitleg, valueCssttrUitleg);
-  localStorage.setItem(cssttrSnappen, valueCssttrSnappen);
-  localStorage.setItem(cssttrCijfer, valueCssttrCijfer);
-};
-
-
-//////// PWA localStorage
-pwaButton.onclick = () => {
-
-  // label text - KEY
-  const pwaDocent = 'PWA docent';
-  const pwaMoeilijk = 'PWA moeilijk';
-  const pwaUitleg = 'PWA uitleg';
-  const pwaSnappen = 'PWA snappen';
-  const pwaCijfer = 'PWA cijfer';
-
-  // input value - VALUE
-  const valuePwaDocent = pwaDocentValue.value;
-  const valuePwaMoeilijk = pwaMoeilijkValue.value;
-  const valuePwaUitleg = pwaUitlegValue.value;
-  const valuePwaSnappen = pwaSnappenValue.value;
-  const valuePwaCijfer = pwaCijferValue.value;
-
-  // local storage
-  localStorage.setItem(pwaDocent, valuePwaDocent);
-  localStorage.setItem(pwaMoeilijk, valuePwaMoeilijk);
-  localStorage.setItem(pwaUitleg, valuePwaUitleg);
-  localStorage.setItem(pwaSnappen, valuePwaSnappen);
-  localStorage.setItem(pwaCijfer, valuePwaCijfer);
-};
-
-
-//////// BT localStorage
-btButton.onclick = () => {
-
-  // label text - KEY
-  const btDocent = 'BT docent';
-  const btMoeilijk = 'BT moeilijk';
-  const btUitleg = 'BT uitleg';
-  const btSnappen = 'BT snappen';
-  const btCijfer = 'BT cijfer';
-
-  // input value - VALUE
-  const valueBtDocent = btDocentValue.value;
-  const valueBtMoeilijk = btMoeilijkValue.value;
-  const valueBtUitleg = btUitlegValue.value;
-  const valueBtSnappen = btSnappenValue.value;
-  const valueBtCijfer = btCijferValue.value;
-
-  // local storage
-  localStorage.setItem(btDocent, valueBtDocent);
-  localStorage.setItem(btMoeilijk, valueBtMoeilijk);
-  localStorage.setItem(btUitleg, valueBtUitleg);
-  localStorage.setItem(btSnappen, valueBtSnappen);
-  localStorage.setItem(btCijfer, valueBtCijfer);
-};
-
-
-//////// RTW localStorage
-rtwButton.onclick = () => {
-
-  // label text - KEY
-  const rtwDocent = 'RTW docent';
-  const rtwMoeilijk = 'RTW moeilijk';
-  const rtwUitleg = 'RTW uitleg';
-  const rtwSnappen = 'RTW snappen';
-  const rtwCijfer = 'RTW cijfer';
-
-  // input value - VALUE
-  const valueRtwDocent = rtwDocentValue.value;
-  const valueRtwMoeilijk = rtwMoeilijkValue.value;
-  const valueRtwUitleg = rtwUitlegValue.value;
-  const valueRtwSnappen = rtwSnappenValue.value;
-  const valueRtwCijfer = rtwCijferValue.value;
-
-  // local storage
-  localStorage.setItem(rtwDocent, valueRtwDocent);
-  localStorage.setItem(rtwMoeilijk, valueRtwMoeilijk);
-  localStorage.setItem(rtwUitleg, valueRtwUitleg);
-  localStorage.setItem(rtwSnappen, valueRtwSnappen);
-  localStorage.setItem(rtwCijfer, valueRtwCijfer);
-};
-
-
-//////// HCD localStorage
-submitButton.onclick = () => {
-
-  // label text - KEY
-  const hcdDocent = 'HCD docent';
-  const hcdMoeilijk = 'HCD moeilijk';
-  const hcdUitleg = 'HCD uitleg';
-  const hcdSnappen = 'HCD snappen';
-  const hcdCijfer = 'HCD cijfer';
-
-  // input value - VALUE
-  const valueHcdDocent = hcdDocentValue.value;
-  const valueHcdMoeilijk = hcdMoeilijkValue.value;
-  const valueHcdUitleg = hcdUitlegValue.value;
-  const valueHcdSnappen = hcdSnappenValue.value;
-  const valueHcdCijfer = hcdCijferValue.value;
-
-  // local storage
-  localStorage.setItem(hcdDocent, valueHcdDocent);
-  localStorage.setItem(hcdMoeilijk, valueHcdMoeilijk);
-  localStorage.setItem(hcdUitleg, valueHcdUitleg);
-  localStorage.setItem(hcdSnappen, valueHcdSnappen);
-  localStorage.setItem(hcdCijfer, valueHcdCijfer);
+    if (item) input.value = item;
+  })
 };
